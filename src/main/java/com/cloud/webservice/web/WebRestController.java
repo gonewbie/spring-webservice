e1a73b9ad8e01ab5a1b808495f2a6b2680205914
Aron Moon <gounewbie@gmail.com>
2019-01-20 20:54:25 +0900

    first commit

---
+++ src/main/java/com/cloud/webservice/web/WebRestController.java
@@ -0,0 +1,14 @@
+package com.cloud.webservice.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "HelloWorld";
+	}
+
+}
