f8751cc9598540433f8e6cd3a350c939fee52be8
Aron Moon <gounewbie@gmail.com>
2019-02-08 01:20:41 +0900

    correct DB config

--- src/main/java/com/cloud/webservice/web/WebRestController.java
+++ src/main/java/com/cloud/webservice/web/WebRestController.java
@@ -33,6 +33,7 @@ public class WebRestController {
 	@GetMapping("/profile")
 	public String getProfile () {
 		return Arrays.stream(env.getActiveProfiles())
+				.filter(e -> "set1".equals(e) || "set2".equals(e))
 				.findFirst()
 				.orElse("");
 	}
